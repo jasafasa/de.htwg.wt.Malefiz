59d2723bb6d80c915164b0166330d50727d1fdcc
TiHau <tim.hauenstein97@gmx.de>
2018-11-21 18:34:53 +0100

    added click function

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,19 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+$(document).ready(function () {
+    console.log("document ready")
+});
+
+$(document).click(function(event) {
+    var target = $(event.target)
+
+    //quit if something else than stone is clicked
+    if (!target.hasClass("stone"))
+        return
+
+    target = target.parent()
+
+    var x = target.attr("x")
+    var y = target.attr("y")
+
+    console.log(x + "  " + y)
+
+});
