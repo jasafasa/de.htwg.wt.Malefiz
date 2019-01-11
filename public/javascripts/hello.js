51e3172f26713885ee494a8053737ff02841e61a
TiHau <tim.hauenstein97@gmx.de>
2019-01-11 19:37:13 +0100

    changed heroku url

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,5 +1,5 @@
 //let wsUri = "ws://localhost:9000/websocket";
-let wsUri = "wss://malefiz-web.herokuapp.com/websocket";
+let wsUri = "wss://malefiz.herokuapp.com/websocket";
 
 function undo() {
     $.ajax(
